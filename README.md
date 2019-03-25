1aab5f99b1ac39de9a54a44222fc18475e0c426f
iPc <syhyz1990@qq.com>
2019-03-25 15:30:38 +0800

    update

--- README.md
+++ README.md
@@ -1,11 +1,15 @@
 # VIP视频解析助手
 ![TamperMonkey v4.5](https://img.shields.io/badge/TamperMonkey-v4.8-brightgreen.svg) ![Chrome x64 v60.4](https://img.shields.io/badge/Chrome%20x64-v73.0-brightgreen.svg) ![Safari v11.0.3](https://img.shields.io/badge/Safari%20-v12.0-brightgreen.svg)
+
+
 ### 安装地址
 [安装地址1](https://greasyfork.org/zh-CN/scripts/380568)
 
 [安装地址2](https://openuserjs.org/scripts/syhyz1990/VIP视频解析助手)
 
 ### 更新日志
+**2019-03-25** : v1.0.3 更新接口和解析描述
+
 **2019-03-20** : v1.0.2 更新接口
 
 **2019-03-19** : v1.0.0 支持各大视频网站VIP视频解析
@@ -15,13 +19,13 @@
 
 ### 使用必读
 
-安装插件后左侧会显示下图按钮，点击后选择线路即可播放
+安装插件后在**视频播放页面**的左侧会显示下图按钮，点击后选择解析线路点击后即可播放
 
 ![插件界面](https://ww1.sinaimg.cn/large/4db689e3ly1g17yt0tksqj21280pdkg0.jpg)
 
 ### 解析测试
 
-优酷：https://v.youku.com/v_show/id_XNDA3MzgwMzQ3Ng==.html
+优酷：https://v.youku.com/v_show/id_XNDA3MzgwMzQ3Ng==
 
 爱奇艺：https://www.iqiyi.com/v_19rro4u4ms.html
 
@@ -31,7 +35,7 @@
 
 搜狐：http://tv.sohu.com/20171020/n600217514.shtml
 
-腾讯：https://v.qq.com/x/cover/6n89qmcgox3zflm.html
+腾讯：https://v.qq.com/x/cover/ha7r9z89i9d234y/n0030j3kuf7.html
 
 pptv：http://v.pptv.com/show/KRsZlv5k1BJ181s.html
 
@@ -41,7 +45,6 @@ m1905：http://vip.1905.com/play/683679.shtml
 
 bilibili：https://www.bilibili.com/video/av14162235
 
-
 ### 捐赠
 如果您觉得脚本对您有帮助，您可以通过支付宝或微信，扫描二维码，捐赠 X元，^_^，谢谢！
  
@@ -49,6 +52,16 @@ bilibili：https://www.bilibili.com/video/av14162235
 
 ### 免责声明 
 本脚本仅在浏览器端运行，源码公开可见，仅做研究使用，不得用于非法获利， 如产生法律纠纷与脚本作者无关!!!
+
+### 常见问题
+**Q: 安装脚本后没显示![](https://ww1.sinaimg.cn/large/4db689e3ly1g1f2hhtq5uj201r01d0f9.jpg)播放图标**
+
+A: 请检查浏览器是否已安装tampermonkey或暴力猴 , 并启用脚本
+
+**Q: 点击链接后无响应 , 或视频集数时间不对**
+
+A: 尝试换用其他解析链接解析 , 集数不对时请点击对应视频网址的集数后再重新解析
+
 
 ### 脚本推荐
 [百度网盘直接下载助手 直链加速版](https://greasyfork.org/zh-CN/scripts/39504)
--- media.user.js
+++ media.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name              VIP视频解析助手
 // @namespace         https://github.com/syhyz1990/media
-// @version           1.0.2
+// @version           1.0.3
 // @icon              https://gw.alicdn.com/tfs/TB1ZvwSycbpK1RjSZFyXXX_qFXa-48-48.ico
 // @description       解析各大视频网站VIP视频，支持优酷，爱奇艺，乐视，腾讯视频，土豆，芒果TV，bilibili，PPTV，暴风，音悦台
 // @author            syhyz1990
@@ -42,41 +42,41 @@
 (function () {
   'use strict';
 
-  GM_addStyle('.h-icon-play {color: #d926b5;fill: #d926b5;height: 80px;width: 80px;position: fixed;z-index: 99999;top: 200px;left: 0;cursor: pointer;}  .h-ol {position: fixed;top: 270px;left: 20px;z-index: 99999;counter-reset: li;list-style: none;font-size: 14px;padding: 0;margin-bottom: 4em;text-shadow: 0 1px 0 rgba(255, 255, 255, .5);display: none;}  .h-ol a {position: relative;display: block;padding: 3px 10px 3px 2em;margin: 0.5em 0;background: #ddd;color: #444;text-decoration: none;border-radius: 0.3em;transition: all 0.3s ease-out;}  .h-ol a:hover {background: #eee;color: #ff6f5c;transition: all 0.3s ease-out;}  .h-ol a::before {content: counter(li);counter-increment: li;position: absolute;left: -1.2em;top: 50%;margin-top: -1.2em;background: #87ceeb;height: 2em;width: 2em;line-height: 2em;border: 0.2em solid #fff;text-align: center;font-weight: bold;border-radius: 2em;}');
+  GM_addStyle('.h-icon-play {color: #d926b5;fill: #d926b5;height: 80px;width: 80px;position: fixed;z-index: 99999;top: 180px;left: 0;cursor: pointer;}  .h-ol {position: fixed;top: 250px;left: 20px;z-index: 99999;counter-reset: li;list-style: none;font-size: 14px;padding: 0;margin-bottom: 4em;text-shadow: 0 1px 0 rgba(255, 255, 255, .5);display: none;}  .h-ol a {position: relative;display: block;padding: 3px 10px 3px 2em;margin: 0.5em 0;background: #ddd;color: #444;text-decoration: none;border-radius: 0.3em;transition: all 0.3s ease-out;}  .h-ol a:hover {background: #eee;color: #ff6f5c;transition: all 0.3s ease-out;}  .h-ol a::before {content: counter(li);counter-increment: li;position: absolute;left: -1.2em;top: 50%;margin-top: -1.2em;background: #87ceeb;height: 2em;width: 2em;line-height: 2em;border: 0.2em solid #fff;text-align: center;font-weight: bold;border-radius: 2em;}');
 
   let api = [
     {name: '金桥解析', url: 'http://jqaaa.com/jx.php?url='},
     {name: '思古解析', url: 'http://api.bbbbbb.me/jx/?url='},
     {name: '思念解析', url: 'http://api.7kki.cn/api/?url='},
-    {name: '超清解析', url: 'https://cdn.yangju.vip/k/?url='},
+    {name: '无名解析', url: 'https://beaacc.com/api.php?url='},
     {name: '拉车解析', url: 'https://jx.lache.me/cc/?url='}];
 
-  function showButton() {
-    if (location.host.match(/youku|iqiyi|le|qq|tudou|mgtv|sohu|acfun|bilibili|pptv|baofeng|yinyuetai/ig)) {
-      let mainButton = '<div class="h-icon-play" title="点击显示解析地址"><svg viewBox="0 0 512 512"><path d="M422.6 193.6c-5.3-45.3-23.3-51.6-59-54 -50.8-3.5-164.3-3.5-215.1 0 -35.7 2.4-53.7 8.7-59 54 -4 33.6-4 91.1 0 124.8 5.3 45.3 23.3 51.6 59 54 50.9 3.5 164.3 3.5 215.1 0 35.7-2.4 53.7-8.7 59-54C426.6 284.8 426.6 227.3 422.6 193.6zM222.2 303.4v-94.6l90.7 47.3L222.2 303.4z"></path></svg></div>';
-      let apiList = '<ol class="h-ol"></ol>';
-      let github = '<iframe src="https://ghbtns.com/github-btn.html?user=syhyz1990&repo=media&type=star&count=true" frameborder="0" scrolling="0" style="height: 20px;max-width: 110px;padding: 0 5px;box-sizing: border-box;margin-top: 10px;"></iframe>';
 
-      $(top.document.body).append(mainButton);
-      $(top.document.body).append(apiList);
+  let main = {
+    showButton: function () {
+      if (location.host.match(/youku|iqiyi|le|qq|tudou|mgtv|sohu|acfun|bilibili|pptv|baofeng|yinyuetai/ig)) {
+        let mainButton = '<div class="h-icon-play" title="点击显示解析地址"><svg viewBox="0 0 512 512"><path d="M422.6 193.6c-5.3-45.3-23.3-51.6-59-54 -50.8-3.5-164.3-3.5-215.1 0 -35.7 2.4-53.7 8.7-59 54 -4 33.6-4 91.1 0 124.8 5.3 45.3 23.3 51.6 59 54 50.9 3.5 164.3 3.5 215.1 0 35.7-2.4 53.7-8.7 59-54C426.6 284.8 426.6 227.3 422.6 193.6zM222.2 303.4v-94.6l90.7 47.3L222.2 303.4z"></path></svg></div>';
+        let apiList = '<ol class="h-ol"></ol>';
+        let github = '<iframe src="https://ghbtns.com/github-btn.html?user=syhyz1990&repo=media&type=star&count=true" frameborder="0" scrolling="0" style="height: 20px;max-width: 110px;padding: 0 5px;box-sizing: border-box;margin-top: 10px;"></iframe>';
 
-      api.forEach((val, index) => {
-        $('.h-ol').append(`<li><a href="${val.url + encodeURI(location.href)}">${val.name}</a></li>`)
-      });
+        $(top.document.body).append(mainButton);
+        $(top.document.body).append(apiList);
 
-      $('.h-ol').append(github);
+        api.forEach((val, index) => {
+          $('.h-ol').append(`<li><a target="_blank" href="${val.url + encodeURI(location.href)}">${val.name}</a></li>`)
+        });
+        //反馈按钮
+        $('.h-ol').append(`<li><a target="_blank" style="color: #999;" href="https://github.com/syhyz1990/media/issues">点击反馈</a></li>`);
+        $('.h-ol').append(github);
 
-      $(top.document.body).on('click', '.h-icon-play', () => {
-        $('.h-ol').toggle();
-      });
+        $(top.document.body).on('click', '.h-icon-play', () => {
+          $('.h-ol').fadeToggle('fast');
+        });
+      }
     }
-  }
-
-  function init() {
-    showButton();
-  }
+  };
 
   $(function () {
-    init();
+    main.showButton();
   });
 })();
