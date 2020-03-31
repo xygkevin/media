e027a1b857752cd8ad72662202c7c6c2874fceae
hyz <syhyz1990@qq.com>
2020-03-31 11:00:26 +0800

    1.0.8

--- media.user.js
+++ media.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name              视频解析助手
 // @namespace         https://github.com/syhyz1990/media
-// @version           1.0.7
+// @version           1.0.8
 // @icon              https://gw.alicdn.com/tfs/TB1ZvwSycbpK1RjSZFyXXX_qFXa-48-48.ico
 // @description       解析各大视频网站视频，支持优酷，爱奇艺，乐视，腾讯视频，土豆，芒果TV，bilibili，PPTV，暴风，音悦台
 // @author            syhyz1990
@@ -30,8 +30,6 @@
 // @match             *://vip.pptv.com/show/*
 // @match             *://v.pptv.com/show/*
 // @match             *://*.baofeng.com/play/*
-// @match             *://v.yinyuetai.com/video/*
-// @match             *://v.yinyuetai.com/playlist/*
 // @match             *://vip.1905.com/play/*
 // @require           https://cdn.bootcss.com/jquery/1.12.4/jquery.min.js
 // @run-at            document-idle
@@ -42,22 +40,80 @@
 (function () {
   'use strict';
 
-  GM_addStyle('.h-icon-play {color: #d926b5;fill: #d926b5;height: 80px;width: 80px;position: fixed;z-index: 99999;top: 180px;left: 0;cursor: pointer;}  .h-ol {position: fixed;top: 250px;left: 20px;z-index: 99999;counter-reset: li;list-style: none;font-size: 14px;padding: 0;margin-bottom: 4em;text-shadow: 0 1px 0 rgba(255, 255, 255, .5);display: none;}  .h-ol a {position: relative;display: block;padding: 3px 10px 3px 2em;margin: 0.5em 0;background: #ddd;color: #444;text-decoration: none;border-radius: 0.3em;transition: all 0.3s ease-out;}  .h-ol a:hover {background: #eee;color: #ff6f5c;transition: all 0.3s ease-out;}  .h-ol a::before {content: counter(li);counter-increment: li;position: absolute;left: -1.2em;top: 50%;margin-top: -1.2em;background: #87ceeb;height: 2em;width: 2em;line-height: 2em;border: 0.2em solid #fff;text-align: center;font-weight: bold;border-radius: 2em;}');
+  GM_addStyle(`
+  .h-icon-play {
+    color: #d926b5;
+    fill: #d926b5;
+    height: 80px;
+    width: 80px;
+    position: fixed;
+    z-index: 99999;
+    top: 10px;
+    left: 0;
+    cursor: pointer;
+  }
+
+  .h-ol {
+    position: fixed;
+    top: 70px;
+    left: 20px;
+    z-index: 99999;
+    counter-reset: li;
+    list-style: none;
+    font-size: 14px;
+    padding: 0;
+    margin-bottom: 4em;
+    text-shadow: 0 1px 0 rgba(255, 255, 255, .5);
+    display: none;
+  }
+
+  .h-ol a {
+    position: relative;
+    display: block;
+    padding: 3px 10px 3px 2em;
+    margin: 0.5em 0;
+    background: #ddd;
+    color: #444;
+    text-decoration: none;
+    border-radius: 0.3em;
+    transition: all 0.3s ease-out;
+  }
+
+  .h-ol a:hover {
+    background: #eee;
+    color: #ff6f5c;
+    transition: all 0.3s ease-out;
+  }
+
+  .h-ol a::before {
+    content: counter(li);
+    counter-increment: li;
+    position: absolute;
+    left: -1.2em;
+    top: 50%;
+    margin-top: -0.9em;
+    background: #87ceeb;
+    height: 1.5em;
+    width: 1.5em;
+    line-height: 1.5em;
+    border: 0.1em solid #fff;
+    text-align: center;
+    font-weight: bold;
+    border-radius: 1.5em;
+  }
+  `);
 
   let api = [
-    {name: '线路1', url: 'http://jx.kukan.vip/?url='},
-    {name: '线路2', url: 'https://www.urlkj.com/?url='},
-    {name: '线路3', url: 'http://ys.480tw.cn/ay/?url='},
-    {name: '线路4', url: 'https://jx.70808.net/?url='},
-    {name: '线路5', url: 'http://demo.baores.com/?url'}];
-
+    {name: '线路1', url: 'http://api.baiyug.vip/index.php?url='},
+    {name: '线路2', url: 'http://www.82190555.com/index/qqvod.php?url='},
+    {name: '线路3', url: 'http://jx.618g.com/?url='},
+    {name: '线路4', url: 'http://2gty.com/apiurl/yun.php?url='},
 
   let main = {
     showButton: function () {
-      if (location.host.match(/youku|iqiyi|le|qq|tudou|mgtv|sohu|acfun|bilibili|pptv|baofeng|yinyuetai/ig)) {
+      if (location.host.match(/youku|iqiyi|le|qq|tudou|mgtv|sohu|acfun|bilibili|pptv|baofeng/ig)) {
         let mainButton = '<div class="h-icon-play" title="点击显示解析地址"><svg viewBox="0 0 512 512"><path d="M422.6 193.6c-5.3-45.3-23.3-51.6-59-54 -50.8-3.5-164.3-3.5-215.1 0 -35.7 2.4-53.7 8.7-59 54 -4 33.6-4 91.1 0 124.8 5.3 45.3 23.3 51.6 59 54 50.9 3.5 164.3 3.5 215.1 0 35.7-2.4 53.7-8.7 59-54C426.6 284.8 426.6 227.3 422.6 193.6zM222.2 303.4v-94.6l90.7 47.3L222.2 303.4z"></path></svg></div>';
         let apiList = '<ol class="h-ol"></ol>';
-        let github = '<iframe src="https://ghbtns.com/github-btn.html?user=syhyz1990&repo=media&type=star&count=true" frameborder="0" scrolling="0" style="height: 20px;max-width: 110px;padding: 0 5px;box-sizing: border-box;margin-top: 10px;"></iframe>';
 
         $(top.document.body).append(mainButton);
         $(top.document.body).append(apiList);
@@ -66,8 +122,7 @@
           $('.h-ol').append(`<li><a target="_blank" href="${val.url + encodeURI(location.href)}">${val.name}</a></li>`)
         });
         //反馈按钮
-        $('.h-ol').append(`<li><a target="_blank" style="color: #999;" href="https://github.com/syhyz1990/media/issues">点击反馈</a></li>`);
-        $('.h-ol').append(github);
+        $('.h-ol').append(`<li><a target="_blank" style="color: #999;" href="https://github.com/syhyz1990/media/issues">反馈</a></li>`);
 
         $(top.document.body).on('click', '.h-icon-play', () => {
           $('.h-ol').fadeToggle('fast');
