b950349ffb308e7c0c6c76b29e7cd98576c6abda
hyz <syhyz1990@qq.com>
2020-03-31 11:10:22 +0800

    1.0.9

--- media.user.js
+++ media.user.js
@@ -1,12 +1,14 @@
 // ==UserScript==
 // @name              视频解析助手
 // @namespace         https://github.com/syhyz1990/media
-// @version           1.0.8
+// @version           1.0.9
 // @icon              https://gw.alicdn.com/tfs/TB1ZvwSycbpK1RjSZFyXXX_qFXa-48-48.ico
 // @description       解析各大视频网站视频，支持优酷，爱奇艺，乐视，腾讯视频，土豆，芒果TV，bilibili，PPTV，暴风，音悦台
 // @author            syhyz1990
 // @license           MIT
 // @supportURL        https://github.com/syhyz1990/media
+// @updateURL         https://raw.githubusercontent.com/syhyz1990/media/master/media.user.js
+// @downloadURL       https://raw.githubusercontent.com/syhyz1990/media/master/media.user.js
 // @match             *://v.youku.com/v_*
 // @match             *://m.youku.com/v*
 // @match             *://m.youku.com/a*
@@ -40,74 +42,13 @@
 (function () {
   'use strict';
 
-  GM_addStyle(`
-  .h-icon-play {
-    color: #d926b5;
-    fill: #d926b5;
-    height: 80px;
-    width: 80px;
-    position: fixed;
-    z-index: 99999;
-    top: 10px;
-    left: 0;
-    cursor: pointer;
-  }
-
-  .h-ol {
-    position: fixed;
-    top: 70px;
-    left: 20px;
-    z-index: 99999;
-    counter-reset: li;
-    list-style: none;
-    font-size: 14px;
-    padding: 0;
-    margin-bottom: 4em;
-    text-shadow: 0 1px 0 rgba(255, 255, 255, .5);
-    display: none;
-  }
-
-  .h-ol a {
-    position: relative;
-    display: block;
-    padding: 3px 10px 3px 2em;
-    margin: 0.5em 0;
-    background: #ddd;
-    color: #444;
-    text-decoration: none;
-    border-radius: 0.3em;
-    transition: all 0.3s ease-out;
-  }
-
-  .h-ol a:hover {
-    background: #eee;
-    color: #ff6f5c;
-    transition: all 0.3s ease-out;
-  }
-
-  .h-ol a::before {
-    content: counter(li);
-    counter-increment: li;
-    position: absolute;
-    left: -1.2em;
-    top: 50%;
-    margin-top: -0.9em;
-    background: #87ceeb;
-    height: 1.5em;
-    width: 1.5em;
-    line-height: 1.5em;
-    border: 0.1em solid #fff;
-    text-align: center;
-    font-weight: bold;
-    border-radius: 1.5em;
-  }
-  `);
+  GM_addStyle(` .h-icon-play { color: #d926b5; fill: #d926b5; height: 80px; width: 80px; position: fixed; z-index: 99999; top: 10px; left: 0; cursor: pointer; } .h-ol { position: fixed; top: 70px; left: 20px; z-index: 99999; counter-reset: li; list-style: none; font-size: 14px; padding: 0; margin-bottom: 4em; text-shadow: 0 1px 0 rgba(255, 255, 255, .5); display: none; } .h-ol a { position: relative; display: block; padding: 3px 10px 3px 2em; margin: 0.5em 0; background: #ddd; color: #444; text-decoration: none; border-radius: 0.3em; transition: all 0.3s ease-out; } .h-ol a:hover { background: #eee; color: #ff6f5c; transition: all 0.3s ease-out; } .h-ol a::before { content: counter(li); counter-increment: li; position: absolute; left: -1.2em; top: 50%; margin-top: -0.9em; background: #87ceeb; height: 1.5em; width: 1.5em; line-height: 1.5em; border: 0.1em solid #fff; text-align: center; font-weight: bold; border-radius: 1.5em; }`);
 
   let api = [
     {name: '线路1', url: 'http://api.baiyug.vip/index.php?url='},
     {name: '线路2', url: 'http://www.82190555.com/index/qqvod.php?url='},
     {name: '线路3', url: 'http://jx.618g.com/?url='},
-    {name: '线路4', url: 'http://2gty.com/apiurl/yun.php?url='},
+    {name: '线路4', url: 'http://2gty.com/apiurl/yun.php?url='}];
 
   let main = {
     showButton: function () {
@@ -122,7 +63,7 @@
           $('.h-ol').append(`<li><a target="_blank" href="${val.url + encodeURI(location.href)}">${val.name}</a></li>`)
         });
         //反馈按钮
-        $('.h-ol').append(`<li><a target="_blank" style="color: #999;" href="https://github.com/syhyz1990/media/issues">反馈</a></li>`);
+        $('.h-ol').append(`<li><a target="_blank" style="color: #999;" href="https://github.com/syhyz1990/media/issues">失效反馈</a></li>`);
 
         $(top.document.body).on('click', '.h-icon-play', () => {
           $('.h-ol').fadeToggle('fast');
